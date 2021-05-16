930be9ba0f68bdcafcd1cda182c1cbc132ffb221
MacGrec <grecmos@gmail.com>
2021-05-16 18:48:04 +0200

    Add new Asserts to functional test

--- tests/Functional/BranchesControllerTest.php
+++ tests/Functional/BranchesControllerTest.php
@@ -9,11 +9,11 @@ use Symfony\Component\HttpFoundation\Response;
 class BranchesControllerTest extends WebTestCase
 {
     const CODE_400_MESSAGE_SELLER_NOT_EXIST = '{"code":400,"message":"Seller not exist"}';
+    const BRANCH_NAME = "Sucursal Madagascar";
 
     public function testCreateBranchSuccessful()
     {
         $client = static::createClient();
-
         $client->request(
             'POST',
             'api/branches/add',
@@ -21,7 +21,7 @@ class BranchesControllerTest extends WebTestCase
             [],
             ['CONTENT_TYPE' => 'application/json'],
             '{
-                "name": "Sucursal Madagascar",
+                "name": "'. self::BRANCH_NAME .'",
                 "location": {
                     "address": "calle una misma",
                     "postal_code": 11170,
@@ -31,7 +31,11 @@ class BranchesControllerTest extends WebTestCase
             }'
         );
 
-        $this->assertSame(Response::HTTP_OK, $client->getResponse()->getStatusCode());
+        $json_response = json_decode($client->getResponse()->getContent());
+        $this->assertSame(Response::HTTP_OK,$client->getResponse()->getStatusCode());
+        $this->assertIsInt($json_response->id);
+        $this->assertIsString($json_response->name);
+        $this->assertSame(self::BRANCH_NAME,$json_response->name);
     }
 
     public function testCreateBranchFail()
@@ -45,10 +49,16 @@ class BranchesControllerTest extends WebTestCase
             [],
             ['CONTENT_TYPE' => 'application/json'],
             '{
-                "name": "Sucursal Madagascar"    
+                "name": "'. self::BRANCH_NAME .'"    
             }'
         );
 
-        $this->assertSame(Response::HTTP_BAD_REQUEST, $client->getResponse()->getStatusCode());
+        $error_message = "Field address missed";
+        $json_response = json_decode($client->getResponse()->getContent());
+        $this->assertSame(Response::HTTP_BAD_REQUEST,$client->getResponse()->getStatusCode());
+        $this->assertIsInt($json_response->code);
+        $this->assertSame(Response::HTTP_BAD_REQUEST,$json_response->code);
+        $this->assertIsString($json_response->message);
+        $this->assertSame($error_message,$json_response->message);
     }
 }
